5f164cf46dc869f1e405732153b6343af4ce55c0
= <muchesia@ubuntu20.04LTS>
2023-01-28 11:50:24 +0300

    changes in feature branch

--- branch_file.c
+++ branch_file.c
@@ -1,12 +1,7 @@
 This is to illustrate git branch
 
-We changed the line
-
-Let's see what will happeno
+We will change this line in master to create conflict
 
 This will remain unchanged
 
 End of file
-
-This is the new end of file from  feature_branch and commited the changes
-Last line
