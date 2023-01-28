d0e744ef6aef3bb3044b969caac4e1d1ab306325
= <muchesia@ubuntu20.04LTS>
2023-01-28 11:41:15 +0300

    updates from feature branch

---
+++ branch_file.c
@@ -0,0 +1,12 @@
+This is to illustrate git branch
+
+We changed the line
+
+Let's see what will happeno
+
+This will remain unchanged
+
+End of file
+
+This is the new end of file from  feature_branch and commited the changes
+Last line
